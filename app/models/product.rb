07feea30d32dae7c592dea9470dc1eb066f43bfa
butuzgol <ButuzGOL@gmail.com>
2012-02-26 12:11:24 -0500

    ajax cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,18 @@ class Product < ActiveRecord::Base
       :message => 'must be a URL for GIF, JPG or PNG image.'
   }
 
+  default_scope :order => 'title'
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+
 end
